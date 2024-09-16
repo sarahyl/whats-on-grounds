65a8887fa151ccd608cf8f9836f4dea33fd359a4
sarahyl <sarah.liu.va@gmail.com>
2024-09-16 17:35:27 -0400

    set up spring boot app

---
+++ springboot_api/src/test/java/project/whatsongrounds/WhatsongroundsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.whatsongrounds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsongroundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
